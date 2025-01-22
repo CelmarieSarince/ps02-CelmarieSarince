9f651c81caf775a6c66b31b638ca3de3dbe47c3f
CelmarieSarince <202280107@psu.palawan.edu.ph>
2025-01-22 12:44:40 +0800

    Update sortreverse.py

--- sortreverse.py
+++ sortreverse.py
@@ -3,7 +3,9 @@ print(sorted(numbers))
 print(sorted(numbers, reverse=True))
 
 numbers = (sorted(numbers, reverse=True))
-
+sorted_numbers = sorted(numbers)
+reversed_numbers = reversed(numbers)
+reversed_sorted_numbers = list(reversed(sorted_numbers))
 
 
 
