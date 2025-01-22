d207fcf572d4340b69da507e7a5c6e022eded936
CelmarieSarince <202280107@psu.palawan.edu.ph>
2025-01-22 13:01:56 +0800

    Update deepcopy_list.py

--- deepcopy_list.py
+++ deepcopy_list.py
@@ -1,3 +1,12 @@
+import copy
+list1 = [1, 2, 3, 4]
+list2 = list1
+list2.append(5)
+print(list1)
+
+list2 = list1[:]
+list2 = copy.copy(list1)
+list2 = copy.deepcopy(list1)
 
 nested_list = [[1,2], [3,4]]
 alias_nested = nested_list
