6a42d091e89bd66fd928e214cdab3348ff9f34b8
CelmarieSarince <202280107@psu.palawan.edu.ph>
2025-01-22 12:35:59 +0800

    Add files via upload

---
+++ sortreverse.py
@@ -0,0 +1,11 @@
+numbers = [5, 2, 9, 1, 5, 6]
+print(sorted(numbers))
+print(sorted(numbers, reverse=True))
+
+numbers = (sorted(numbers, reverse=True))
+
+
+
+
+print("Sorted list:", sorted_numbers)
+print("Reversed list:", reversed_numbers)
