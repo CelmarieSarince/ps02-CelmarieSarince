a803e86e36bc937a75312082db5457642ccca760
CelmarieSarince <202280107@psu.palawan.edu.ph>
2025-01-22 12:47:53 +0800

    Update concat_compare.py

--- concat_compare.py
+++ concat_compare.py
@@ -1,10 +1,10 @@
 import time
 start = time.time()
 result = ""
-for i in range(1, 10001):
+for i in range(1, 100001):
     result += str(i)
     print("+= Time:", time.time() - start)
 
 start = time.time()
-result = "".join(str(i)for i in range(1, 10001))
+result = "".join(str(i)for i in range(1, 100001))
 print("Using Join Method:", time.time() - start)
